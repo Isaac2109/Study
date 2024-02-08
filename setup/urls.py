1f8b97e1888b6f8e42814f4437c8e2b2f4b75224
Isaac2109 <lrsiazevedo2018@gmail.com>
2024-02-08 17:41:22 -0300

    Página de teste terminada

--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from study.views import home, result
+from study.views import home, test, result
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('result', result, name='result')
+    path('test/', test, name='test'),
+    path('result/', result, name='result')
 ]
--- study/views.py
+++ study/views.py
@@ -1,37 +1,40 @@
 from django.urls import reverse
 from django.shortcuts import render, HttpResponseRedirect
-from .forms import MeuForm
 from api import consulta_api
 
 def home(request):
     if request.method == 'POST':
-        form = MeuForm(request.POST)
-        if form.is_valid():
-            assunto = form.cleaned_data['subject']
-            dificuldade = form.cleaned_data['difficulty']
-            qtd_questoes = form.cleaned_data['number']
-
-            consulta = consulta_api(dificuldade, assunto, qtd_questoes)  
-
-            contexto = {
-                'qtd_questoes': qtd_questoes
-            }
-
-            for num in range(1, int(qtd_questoes) + 1):
-                contexto.update({f"cabecalho_{num}": consulta[f"{num}"]["cabeçalho"]})
-                contexto.update({f"alter_a_{num}": consulta[f"{num}"]["alternativa_a"]})
-                contexto.update({f"alter_b_{num}": consulta[f"{num}"]["alternativa_b"]})
-                contexto.update({f"alter_c_{num}": consulta[f"{num}"]["alternativa_c"]})
-                contexto.update({f"alter_d_{num}": consulta[f"{num}"]["alternativa_d"]})
-                contexto.update({f"resp{num}": consulta[f"{num}"]["resposta"]})
-
-                
-
-            return render(request, 'study/test.html', context=contexto)
-
+        return HttpResponseRedirect(reverse(test))
     else:
-        form = MeuForm()
-        return render(request, 'study/home.html', {'form': form})
+        return render(request, 'study/home.html')
     
+def test(request):
+
+    if request.method == 'POST':
+        return HttpResponseRedirect(reverse(result))
+    
+    else:
+    
+        assunto = request.POST.get('assunto')
+        dificuldade = request.POST.get('dificuldade')
+        qtd_questoes = request.POST.get('qtd_questoes')
+    
+        consulta = consulta_api(dificuldade, assunto, qtd_questoes)  
+    
+        contexto = {
+                'qtd_questoes': qtd_questoes,
+                'n': range(1, int(qtd_questoes) + 1)
+            }
+        for num in range(1, int(qtd_questoes) + 1):
+            contexto.update({f"cabecalho_{num}": consulta[f"{num}"]["cabeçalho"]})
+            contexto.update({f"alter_a_{num}": consulta[f"{num}"]["alternativa_a"]})
+            contexto.update({f"alter_b_{num}": consulta[f"{num}"]["alternativa_b"]})
+            contexto.update({f"alter_c_{num}": consulta[f"{num}"]["alternativa_c"]})
+            contexto.update({f"alter_d_{num}": consulta[f"{num}"]["alternativa_d"]})
+            contexto.update({f"resp{num}": consulta[f"{num}"]["resposta"]})
+    
+        return render(request, 'study/test.html', context=contexto)
+
+
 def result(request):
-    pass
+    return render(request, 'study/result.html')
